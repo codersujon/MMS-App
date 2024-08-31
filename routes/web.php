ba0b876131ea0c186221c579ed2247167753ca64
unknown <csesujon155@gmail.com>
2024-08-30 21:01:07 -0700

    routes group

--- routes/web.php
+++ routes/web.php
@@ -15,35 +15,49 @@ Route::get('/dashboard', function () {
     return view('backend.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Members
-Route::get('/members', [MemberController::class, 'index'])->name('members.index');
-Route::post('/members/store', [MemberController::class, 'store'])->name('members.store');
-Route::get('/members/list', [MemberController::class, 'memberList'])->name('members.list');
-Route::get('/members/destroy/{id}', [MemberController::class, 'destroy'])->name('members.destroy');
+
+Route::middleware('auth')->group(function(){
+    Route::controller(MemberController::class)->group(function(){
+        Route::get('/members', 'index')->name('members.index');
+        Route::post('/members/store', 'store')->name('members.store');
+        Route::get('/members/list', 'memberList')->name('members.list');
+        Route::get('/members/destroy/{id}','destroy')->name('members.destroy');
+    });
+});
+
 
 // Members Plans
-Route::get('/members/plans', [MemberPlanController::class, 'index'])->name('members.plans.index');
-Route::post('/members/plans/store', [MemberPlanController::class, 'store'])->name('members.plans.store');
-Route::get('/members/plans/all', [MemberPlanController::class, 'allPlans'])->name('members.plans.all');
-Route::get('/members/plans/destroy/{id}', [MemberPlanController::class, 'destroyPlans'])->name('members.plans.destroy');
+Route::middleware('auth')->group(function(){
+    Route::controller(MemberPlanController::class)->group(function(){
+        Route::get('/members/plans', 'index')->name('members.plans.index');
+        Route::post('/members/plans/store', 'store')->name('members.plans.store');
+        Route::get('/members/plans/all', 'allPlans')->name('members.plans.all');
+        Route::get('/members/plans/destroy/{id}', 'destroyPlans')->name('members.plans.destroy');
+    });
+});
 
 
 // Penalty Settings
-Route::get('/penalty/settings', [PenaltySettingController::class, 'index'])->name('penalty.settings.index');
-Route::post('/penalty/store', [PenaltySettingController::class, 'store'])->name('penalty.settings.store');
-Route::get('/penalty/all', [PenaltySettingController::class, 'allPenalty'])->name('penalty.all');
-Route::get('/penalty/destroy/{id}', [PenaltySettingController::class, 'penaltyDestroy'])->name('penalty.destroy');
+Route::middleware('auth')->group(function(){
+    Route::controller(PenaltySettingController::class)->group(function(){
+        Route::get('/penalty/settings', 'index')->name('penalty.settings.index');
+        Route::post('/penalty/store', 'store')->name('penalty.settings.store');
+        Route::get('/penalty/all', 'allPenalty')->name('penalty.all');
+        Route::get('/penalty/destroy/{id}', 'penaltyDestroy')->name('penalty.destroy');
+    });
+});
 
 
 // Payments
-Route::get('/payments/all', [PaymentController::class, 'index'])->name('payments.all.index');
-Route::post('/payments/store', [PaymentController::class, 'store'])->name('payments.store');
-Route::get('/payments/list', [PaymentController::class, 'allPayments'])->name('payments.list');
-Route::get('/payments/destroy/{id}', [PaymentController::class, 'destroy'])->name('payments.destroy');
-
-
-Route::get('/get-installment/{id}', [PaymentController::class, 'getInstallment'])->name('get-installment');
-
+Route::middleware('auth')->group(function(){
+    Route::controller(PaymentController::class)->group(function(){
+        Route::get('/payments/all', 'index')->name('payments.all.index');
+        Route::post('/payments/store', 'store')->name('payments.store');
+        Route::get('/payments/list', 'allPayments')->name('payments.list');
+        Route::get('/payments/destroy/{id}', 'destroy')->name('payments.destroy');
+        Route::get('/get-installment/{id}', 'getInstallment')->name('get-installment');
+    });
+});
 
 
 Route::middleware('auth')->group(function () {
