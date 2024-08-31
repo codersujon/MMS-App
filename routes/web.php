a03740b6af5ff4bb3a4779f3c608452f24109e7d
unknown <csesujon155@gmail.com>
2024-08-30 21:02:22 -0700

    routes group

--- routes/web.php
+++ routes/web.php
@@ -59,7 +59,6 @@ Route::middleware('auth')->group(function(){
     });
 });
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
